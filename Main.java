ef64dc3c51ea091e5b87d84658859217cd995813
Mori <35340393+Irom1@users.noreply.github.com>
2023-01-30 16:33:45 +0000

    partially working

--- Main.java
+++ Main.java
@@ -1,10 +1,57 @@
-class Main {
+import java.util.Scanner;
+
+// clear && javac Main.java && javac RegEx.java && java Main
+
+/*
+#, aac, acc, abc, ac, abbc, abbbc, abbbbc, aabc, accb
+a.c
+a[ab]c
+a[^ab]c
+ab*c
+ab{2,4}c
+
+1. aac, acc, abc
+2. aac, abc
+3. acc
+4. ac, abc, abbc, abbbc, abbbbc
+5. abbc, abbbc, abbbbc
+
+#, xx, yz, xz, xy, xyz, xxyz, xyyz, yzz, xxxyz
+x[xyz]yz
+x.z
+x[^y]z
+x{1,4}yz
+x*yz
+
+1. xxyz, xyyz
+2. xyz
+3. NONE
+4. xyz, xxyz, xxxyz
+5. yz, xyz, xxyz, xxxyz
+*/
+
+public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    RegEx r = new RegEx("#, aac, acc, abc, ac, abbc, abbbc, abbbbc, aabc, accb");
-    System.out.println(r.findAllMatches("a.c"));
-    System.out.println(r.findAllMatches("ab*c"));
-    System.out.println(r.findAllMatches("a[ab]c"));
-    
+    System.out.println("\nACSL Reg Exp by Marcus & Mori\n");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the 10 possible strings (hit enter for defualt set): ");
+    String options = input.nextLine();
+    if (options.equals("")) {
+      System.out.println("\nTesting Strings\n");
+      RegEx r = new RegEx("#, aac, acc, abc, ac, abbc, abbbc, abbbbc, aabc, accb");
+      System.out.println(r.findAllMatches("a.c"));
+      System.out.println(r.findAllMatches("ab*c"));
+      System.out.println(r.findAllMatches("a[ab]c"));
+      System.out.println(r.findAllMatches("a[^ab]c"));
+      System.out.println(r.findAllMatches("ab{2,4}c"));
+    } else {
+      RegEx r = new RegEx(options);
+      System.out.println("\nEnter the expressions to find which match: ");
+      for (int i = 0; i < 5; i++) {
+        String expression = input.nextLine();
+        System.out.println(r.findAllMatches(expression) + "\n");
+      }
+    }
+    input.close();
   }
 }
--- RegEx.java
+++ RegEx.java
@@ -6,12 +6,17 @@ public class RegEx {
     }
 
     public String findAllMatches(String expression) {
-        System.out.println("Processing " + expression);
+        //System.out.println("Processing " + expression);
         String result = "";
         for (String option : options) {
             if (findMatch(expression, option)) {
                 result += option + ", ";
             }
+        }
+        if (result.length() > 0) {
+            result = result.substring(0, result.length() - 2);
+        } else {
+            result = "NONE";
         }
         return result;
     }
@@ -47,10 +52,12 @@ public class RegEx {
     }
 
     private boolean bracketMatch(String expression, String test) {
-        // Matches a single character contained within the brackets. Ex: [abc] matches a, b, or c.
+        // Matches a single character contained within the brackets. Ex: [abc] matches
+        // a, b, or c.
         String possibleLetters = expression.substring(expression.indexOf("[") + 1, expression.indexOf("]"));
-        if(test.length() == (expression.substring(0, expression.indexOf("[")) + expression.substring(expression.indexOf("["))).length() + 1){
-            if (possibleLetters.indexOf(test.substring(expression.indexOf("["), expression.indexOf("[") + 1)) != -1){
+        if (test.length() == (expression.substring(0, expression.indexOf("["))
+                + expression.substring(expression.indexOf("]"))).length()) {
+            if (possibleLetters.indexOf(test.substring(expression.indexOf("["), expression.indexOf("[") + 1)) != -1) {
                 return true;
             }
         }
@@ -60,6 +67,14 @@ public class RegEx {
     private boolean bracketCarrotMatch(String expression, String test) {
         // Matches any single character not contained within the brackets. [^abc]
         // matches any character other than a, b, or c.
+        String possibleLetters = expression.substring(expression.indexOf("[") + 1, expression.indexOf("]"));
+        if (test.length() == (expression.substring(0, expression.indexOf("["))
+                + expression.substring(expression.indexOf("]"))).length()) {
+            if (!(possibleLetters
+                    .indexOf(test.substring(expression.indexOf("["), expression.indexOf("[") + 1)) != -1)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -68,17 +83,19 @@ public class RegEx {
         // aab, aaab, ...etc.
         char prevChar = expression.charAt(expression.indexOf("*") - 1);
         String beforeStar = expression.substring(0, expression.indexOf("*") - 1);
-        if(beforeStar.equals(test.substring(0, beforeStar.length()))){
-            for(int i = beforeStar.length(); i < test.length(); i++){
-                if(test.charAt(i) == prevChar){
+        String stuffAfterStar = expression.substring(expression.indexOf("*") + 1);
+        if (beforeStar.equals(test.substring(0, beforeStar.length()))) {
+            for (int i = beforeStar.length(); i < test.length() - stuffAfterStar.length(); i++) {
+                if (test.charAt(i) == prevChar) {
                     continue;
                 } else {
                     return false;
                 }
             }
+        } else {
+            return false;
         }
-        String stuffAfterStar = expression.substring(expression.indexOf("*") + 1);
-        if(!stuffAfterStar.equals(test.substring(test.length() - stuffAfterStar.length()))){
+        if (!stuffAfterStar.equals(test.substring(test.length() - stuffAfterStar.length()))) {
             return false;
         }
         return true;
@@ -87,6 +104,36 @@ public class RegEx {
     private boolean curlyBracketMatch(String expression, String test) {
         // Matches the preceding character at least m but not more than n times. Ex:
         // a{3,5} matches aaa, aaaa, or aaaaa.
+        
+        // find repreatign character (before curly brackets)
+        int repeatedCharIndex = expression.indexOf("{") - 1;
+        char repeatedChar = expression.charAt(repeatedCharIndex);
+        // find m and n
+        int m = Integer.parseInt(expression.substring(expression.indexOf("{") + 1, expression.indexOf(",")));
+        int n = Integer.parseInt(expression.substring(expression.indexOf(",") + 1, expression.indexOf("}")));
+        // find substring before repeating character
+        String beforeRepeatingChar = expression.substring(0, repeatedCharIndex);
+        // find substring after curly brackets
+        String afterRepeatingChar = expression.substring(expression.indexOf("}") + 1);
+        // generate all possible strings with m to n repeating characters
+        String[] possibleStrings = new String[n - m + 1];
+        int index = 0;
+        while (m <= n) {
+            String possibility = beforeRepeatingChar;
+            for(int i = 0; i < m; i++){
+                possibility += repeatedChar;
+            }
+            possibility += afterRepeatingChar;
+            possibleStrings[index] = possibility;
+            index++;
+            m++;
+        }
+        // check if test is one of the possible strings
+        for(String possibility : possibleStrings){
+            if (possibility.equals(test)) {
+                return true;
+            }
+        }
         return false;
     }
 }
